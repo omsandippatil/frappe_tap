0d1ed9e7a4a549499fd9e8a7b56dea31eff4c583
nithya <nithyakv1989@gmail.com>
2025-08-13 17:44:13 +0530

    test

--- tap_lms/tests/test_feedback_request.py
+++ tap_lms/tests/test_feedback_request.py
@@ -1,94 +1,81 @@
-# Copyright (c) 2024, Tech4dev and Contributors
-# See License.txt
-
 import pytest
 import sys
 from unittest.mock import Mock
 
-
 def test_feedback_request_coverage():
     """
-    Minimal test to achieve 100% coverage for test_feedback_request.py
-    Covers the import and class definition lines
+    Minimal test to achieve 100% coverage for feedback_request.py
+    Covers lines 5, 7, and 8
     """
     
-    # Mock frappe module and its components
-    class MockFrappeTestCase:
+    # Mock frappe module
+    class MockDocument:
         def __init__(self, *args, **kwargs):
             pass
     
     mock_frappe = Mock()
-    mock_frappe.tests = Mock()
-    mock_frappe.tests.utils = Mock()
-    mock_frappe.tests.utils.FrappeTestCase = MockFrappeTestCase
+    mock_frappe.model = Mock()
+    mock_frappe.model.document = Mock()
+    mock_frappe.model.document.Document = MockDocument
     
     sys.modules['frappe'] = mock_frappe
-    sys.modules['frappe.tests'] = mock_frappe.tests
-    sys.modules['frappe.tests.utils'] = mock_frappe.tests.utils
+    sys.modules['frappe.model'] = mock_frappe.model
+    sys.modules['frappe.model.document'] = mock_frappe.model.document
     
-    # Import the test class - this covers the import and class definition lines
-    try:
-        from tap_lms.tap_lms.doctype.feedback_request.test_feedback_request import TestFeedbackRequest
-        test_instance = TestFeedbackRequest()
-        
-        # Basic assertions
-        assert test_instance is not None
-        assert TestFeedbackRequest.__name__ == 'TestFeedbackRequest'
-        assert isinstance(test_instance, TestFeedbackRequest)
-        
-    except ImportError:
-        # If import fails, we'll create a mock test
-        pass
+    # Import and instantiate - this covers all 3 lines
+    from tap_lms.tap_lms.doctype.feedback_request.feedback_request import FeedbackRequest
+    feedback_request = FeedbackRequest()
+    
+    # Basic assertions
+    assert feedback_request is not None
+    assert FeedbackRequest.__name__ == 'FeedbackRequest'
+    assert isinstance(feedback_request, FeedbackRequest)
 
 
 def test_feedback_request_inheritance():
-    """Test TestFeedbackRequest inherits properly"""
-    # Mock the frappe module first
-    class MockFrappeTestCase:
-        pass
-    
-    mock_frappe = Mock()
-    mock_frappe.tests = Mock()
-    mock_frappe.tests.utils = Mock()
-    mock_frappe.tests.utils.FrappeTestCase = MockFrappeTestCase
-    
-    sys.modules['frappe'] = mock_frappe
-    sys.modules['frappe.tests'] = mock_frappe.tests
-    sys.modules['frappe.tests.utils'] = mock_frappe.tests.utils
-    
-    try:
-        from tap_lms.tap_lms.doctype.feedback_request.test_feedback_request import TestFeedbackRequest
-        test_instance = TestFeedbackRequest()
-        assert test_instance is not None
-    except ImportError:
-        # Handle import error gracefully
-        assert True
+    """Test FeedbackRequest inherits from Document"""
+    from tap_lms.tap_lms.doctype.feedback_request.feedback_request import FeedbackRequest
+    feedback_request = FeedbackRequest()
+    assert feedback_request is not None
 
 
 def test_feedback_request_multiple_instances():
-    """Test multiple TestFeedbackRequest instances"""
-    # Mock the frappe module
-    class MockFrappeTestCase:
-        pass
+    """Test multiple FeedbackRequest instances"""
+    from tap_lms.tap_lms.doctype.feedback_request.feedback_request import FeedbackRequest
     
-    mock_frappe = Mock()
-    mock_frappe.tests = Mock()
-    mock_frappe.tests.utils = Mock()
-    mock_frappe.tests.utils.FrappeTestCase = MockFrappeTestCase
+    feedback_request1 = FeedbackRequest()
+    feedback_request2 = FeedbackRequest()
     
-    sys.modules['frappe'] = mock_frappe
-    sys.modules['frappe.tests'] = mock_frappe.tests
-    sys.modules['frappe.tests.utils'] = mock_frappe.tests.utils
+    assert feedback_request1 is not None
+    assert feedback_request2 is not None
+    assert feedback_request1 is not feedback_request2
+
+
+def test_feedback_request_class_attributes():
+    """Test FeedbackRequest class attributes and methods"""
+    from tap_lms.tap_lms.doctype.feedback_request.feedback_request import FeedbackRequest
     
-    try:
-        from tap_lms.tap_lms.doctype.feedback_request.test_feedback_request import TestFeedbackRequest
-        
-        test1 = TestFeedbackRequest()
-        test2 = TestFeedbackRequest()
-        
-        assert test1 is not None
-        assert test2 is not None
-        assert test1 is not test2
-    except ImportError:
-        # Handle import error gracefully
-        assert True
+    # Test class name
+    assert hasattr(FeedbackRequest, '__name__')
+    assert FeedbackRequest.__name__ == 'FeedbackRequest'
+    
+    # Test instantiation
+    feedback_request = FeedbackRequest()
+    assert feedback_request.__class__.__name__ == 'FeedbackRequest'
+
+
+def test_feedback_request_with_args():
+    """Test FeedbackRequest instantiation with arguments"""
+    from tap_lms.tap_lms.doctype.feedback_request.feedback_request import FeedbackRequest
+    
+    # Test with positional arguments
+    feedback_request1 = FeedbackRequest("test_arg")
+    assert feedback_request1 is not None
+    
+    # Test with keyword arguments
+    feedback_request2 = FeedbackRequest(name="test_feedback")
+    assert feedback_request2 is not None
+    
+    # Test with both
+    feedback_request3 = FeedbackRequest("test_arg", name="test_feedback")
+    assert feedback_request3 is not None
