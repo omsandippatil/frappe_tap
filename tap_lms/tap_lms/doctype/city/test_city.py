2ead0f98094058b3f9038e7cd6ab42d02b6a1e74
AMARTHA <amartha2704@gmail.com>
2025-08-18 11:15:46 +0530

    test

--- tap_lms/tap_lms/doctype/city/test_city.py
+++ tap_lms/tap_lms/doctype/city/test_city.py
@@ -1,48 +1,9 @@
-# # Copyright (c) 2024, Techt4dev and Contributors
-# # See license.txt
+# Copyright (c) 2024, Techt4dev and Contributors
+# See license.txt
 
-# # import frappe
-# from frappe.tests.utils import FrappeTestCase
-
-
-# class TestCity(FrappeTestCase):
-# 	pass
+# import frappe
+from frappe.tests.utils import FrappeTestCase
 
 
-import pytest
-import sys
-from unittest.mock import Mock
-
-def test_city_coverage():
-    """
-    Minimal test to achieve 100% coverage for city.py
-    Covers lines 5, 7, and 8
-    """
-    
-    # Mock frappe module
-    class MockDocument:
-        def __init__(self, *args, **kwargs):
-            pass
-    
-    mock_frappe = Mock()
-    mock_frappe.model = Mock()
-    mock_frappe.model.document = Mock()
-    mock_frappe.model.document.Document = MockDocument
-    
-    sys.modules['frappe'] = mock_frappe
-    sys.modules['frappe.model'] = mock_frappe.model
-    sys.modules['frappe.model.document'] = mock_frappe.model.document
-    
-    # Import and instantiate - this covers all 3 lines
-    from tap_lms.tap_lms.doctype.city.city import City
-    city = City()
-    
-    # Basic assertions
-    assert city is not None
-    assert City.__name__ == 'City'
-    assert isinstance(city, City)
-
-
-
-
-
+class TestCity(FrappeTestCase):
+	pass
